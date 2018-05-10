7fbe1281153fafd38781833fb951fb447310249a
MarcinOsinski <marcin.osinski@o2.pl>
2018-05-10 11:45:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,128 +9,140 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "mcp2515.h"
 #include "spi.h"
 #include "can.h"
 #include "makra.h"
 
-void hello1()
-{
-	ZERUJ(PORTC,DDC0);
-	_delay_ms(500);
-	USTAW(PORTC,DDC0);
 
-	ZERUJ(PORTC,DDC1);
-	_delay_ms(500);
-	USTAW(PORTC,DDC1);
+volatile uint8_t flag1 = 0;
+volatile uint8_t flag2 = 0;
+volatile uint8_t flag3 = 0;
 
-	ZERUJ(PORTC,DDC2);
-	_delay_ms(500);
-	USTAW(PORTC,DDC2);
+volatile uint8_t timer0;
 
-	ZERUJ(PORTC,DDC5);
-	_delay_ms(500);
-	USTAW(PORTC,DDC5);
-}
 
-void hello2()
-{
-	ZERUJ(PORTC,DDC1);
-	_delay_ms(500);
-	USTAW(PORTC,DDC1);
+void hello();
+void resetCounter1();
 
-	ZERUJ(PORTC,DDC0);
-	_delay_ms(500);
-	USTAW(PORTC,DDC0);
-
-	ZERUJ(PORTC,DDC2);
-	_delay_ms(500);
-	USTAW(PORTC,DDC2);
-}
 
 int main()
 {
-	// piny 0,1,2,5 w porcie C jako wyjścia
 	DDRC |= ((1<<DDC0)|(1<<DDC1)|(1<<DDC2)|(1<<DDC5));
-	// aktywne stanem niskim
 	PORTC |= ((1<<DDC0)|(1<<DDC1)|(1<<DDC2)|(1<<DDC5));
-	// pin 2 portu D (INT0) jako wejście
 	DDRD |= (0<<DDD2);
-	// pull-up
 	PORTD |= (1<<DDD2);
 
-	sei();	// włącz przerwania globalnie
-
-	GICR |= (1 << INT0);	// włącz przerwania INT0 (aktywne stanem niskim)
-
-	hello();
+	OCR1A = 0xffff;
+	TCCR1B |= (1<<WGM12);
+	TIMSK |= ((1<<OCIE1A)|(1<<TOIE0));
 
 	SPI_init();
 	mcp2515_init();
 
+	hello();
+
+	sei();
+	GICR |= (1 << INT0);
+
+	COUNTER0_START;
+	COUNTER1_START;
 
 	while(1)
 	{
-		// sterowanie LED-ami
-		if(flaga1)
-			ZERUJ(PORTC, DDC0);
-		if(!flaga1)
-			USTAW(PORTC, DDC0);
+		if(flag1 == 1)
+			CLEAR(PORTC,DDC0);
+		else
+			SET(PORTC,DDC0);
 
-		if(flaga2)
-			ZERUJ(PORTC, DDC1);
-		if(!flaga2)
-			USTAW(PORTC, DDC1);
+		if(flag2 == 1)
+			CLEAR(PORTC,DDC1);
+		else
+			SET(PORTC,DDC1);
 
-		if(flaga3)
-			ZERUJ(PORTC, DDC2);
-		if(!flaga3)
-			USTAW(PORTC, DDC2);
+		if(flag3 == 1)
+			CLEAR(PORTC,DDC2);
+		else
+			SET(PORTC,DDC2);
 
-		if(flaga1 && flaga2 && flaga3)
+
+		if(flag1 && flag2 && flag3)
+			CLEAR(PORTC,DDC5);
+		else
+			SET(PORTC,DDC5);
+
+		if(timer0 > 250)
 		{
-			// włącz przekaźnik
-			ZERUJ(PORTC,DDC5);
-		}
+			timer0 = 0;
 
-		if(!(flaga1 && flaga2 && flaga3))
-		{
-			// wyłącz przekaźnik
-			USTAW(PORTC,DDC5);
+			flag1 = 0;
+			flag2 = 0;
 		}
 	}
 
 }
 
+
 ISR(INT0_vect)
 {
-	// bufor pełny - procedura odbierania wiadomości;
-	/*if(mcp2515_readRegister(CANINTF) & (1<<MERRF))
-		hello2();
-
-	if(mcp2515_readRegister(CANINTF) & (1<<RX1IF))
-		hello1();*/
-
-	hello2();
-
-	switch(CAN_ktoraWiadomosc())
+	switch(CAN_whichMessage())
 	{
-		case 0:
-		// error
-		break;
-
 		case 1:
-			CAN_wiadomosc1(flaga1, flaga2);
+			CAN_message1(&flag1, &flag2);
+			timer0 = 0;
 			break;
 
 		case 2:
-			CAN_wiadomosc2(flaga3);
+			CAN_message2(&flag3);
+			resetCounter1();
 			break;
-
 	}
 
-	// czyszczenie wszystkich flag przerwań - koniec obsługi przerwania
 	mcp2515_writeRegister(CANINTF, 0x00);
+}
 
+ISR(TIMER0_OVF_vect)
+{
+	timer0++;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	flag3 = 0;
+}
+
+void hello()
+{
+	CLEAR(PORTC, DDC0);
+	CLEAR(PORTC, DDC1);
+	CLEAR(PORTC, DDC2);
+	_delay_ms(1000);
+	SET(PORTC, DDC0);
+	SET(PORTC, DDC1);
+	SET(PORTC, DDC2);
+	_delay_ms(500);
+	CLEAR(PORTC, DDC0);
+	CLEAR(PORTC, DDC1);
+	CLEAR(PORTC, DDC2);
+	_delay_ms(1000);
+	SET(PORTC, DDC0);
+	SET(PORTC, DDC1);
+	SET(PORTC, DDC2);
+	_delay_ms(1000);
+}
+
+void resetCounter1()
+{
+	 unsigned char sreg;
+	 /* Save Global Interrupt Flag */
+	 sreg = SREG;
+	 /* Disable interrupts */
+	 cli();
+	 TCNT1  = 0;
+	 /* Restore Global Interrupt Flag */
+	 SREG = sreg;
 }
 

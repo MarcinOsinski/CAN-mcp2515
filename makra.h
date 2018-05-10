fe1972ebd4a2d6e516a72cf83a3d982413bdd2b1
MarcinOsinski <marcin.osinski@o2.pl>
2018-05-10 11:46:42 +0200

    Update makra.h

--- makra.h
+++ makra.h
@@ -1,17 +1,13 @@
-/*
- * makra.h
- *
- *  Created on: 30 gru 2017
- *      Author: rysiek
- */
-
 #ifndef MAKRA_H_
 #define MAKRA_H_
 
-#define ZERUJ(PORT,NUMER)				PORT &= ~(1 << NUMER)
-#define USTAW(PORT,NUMER)				PORT |=  (1 << NUMER)
+#define CLEAR(PORT,NUMER)				PORT &= ~(1 << NUMER)
+#define SET(PORT,NUMER)					PORT |=  (1 << NUMER)
 
-#define SPRAWDZ_1_NA(PIN,NUMER)			PIN & (1 << NUMER)
-#define SPRAWDZ_0_NA(PIN,NUMER)			!(PIN & (1 << NUMER))
+#define CHECK_1_AT(REGISTER,PIN)		REGISTER & (1 << PIN)
+#define CHECK_0_AT(REGISTER,PIN)		!(REGISTER & (1 << PIN))
+
+#define COUNTER0_START	TCCR0|=((1<<CS02)|(1<<CS00))
+#define COUNTER1_START	TCCR1B|=((1<<CS02)|(1<<CS00))
 
 #endif /* MAKRA_H_ */
